9d025afcc6e12b720977ff67bb7cb4eebef2346f
Vasiliy Rodin <rodinvasiliy@gmail.com>
2016-03-01 10:12:05 -0800

    Initial Commit

---
+++ app/src/main/java/com/vasiliyrodin/bobaselector/MainActivity.java
@@ -0,0 +1,35 @@
+package com.vasiliyrodin.bobaselector;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void makeMeBoba(View view){
+        String randomDrink = selectRandomBoba();
+        // send result of randomBoba as intent to next Screen.
+
+    }
+
+    public String selectRandomBoba(){
+        String[] teaFlavor = {"Assam", "Toffee", "Earl Grey", "High Mountain Oolong","Peach Oolong", "Dark Roast Oolong", "Jasmine","Osmanthus", "Hojicha", "Rose" , "Ceylon", "Lychee", "White Grape Oolong", "Matcha Green Tea Latte"};
+        String[] toppings = {"1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+        int teaFlavorLength = teaFlavor.length;
+        int toppingsLength  = toppings.length;
+
+
+        Random random = new Random();
+        String drink = teaFlavor[random.nextInt(teaFlavorLength)] + " with " + toppings[random.nextInt(toppingsLength)];
+        return drink;
+    }
+}
