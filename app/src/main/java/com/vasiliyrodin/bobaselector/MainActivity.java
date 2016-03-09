0d970d74de98a60f1f304780f082400bb47360ab
Vasiliy Rodin <rodinvasiliy@gmail.com>
2016-03-08 17:49:02 -0800

    Added spinner with correct choices.

--- app/src/main/java/com/vasiliyrodin/bobaselector/MainActivity.java
+++ app/src/main/java/com/vasiliyrodin/bobaselector/MainActivity.java
@@ -5,14 +5,34 @@ import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
 import java.util.Random;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    String[] bobaPlaces;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bobaPlaces = getResources().getStringArray(R.array.bobaPlace_array);
+        Spinner s1 = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, bobaPlaces);
+
+        s1.setAdapter(adapter);
+        s1.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                int index = arg0.getSelectedItemPosition();
+                Toast.makeText(getBaseContext(),"You have selected: " + bobaPlaces[index], Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {}
+        });
     }
 
     public void makeMeBoba(View view){
